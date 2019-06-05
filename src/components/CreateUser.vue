f874af68453ec514e04cc8bc2c4ed5002fefd50b
Peter Nguyen <ptnguyen1987@gmail.com>
2019-06-05 15:50:24 +0700

    basic form

--- src/components/CreateUser.vue
+++ src/components/CreateUser.vue
@@ -1,27 +1,69 @@
 <template>
   <div>
     <h2>Create User Component</h2>
-    <sui-form>
+    <sui-form form @submit.prevent="handleSubmit">
       <sui-form-field>
-        <label>Name <input type="text" id="name" placeholder="Name"/></label>
+        <label
+          >Name
+          <input
+            type="text"
+            data-name
+            placeholder="Name"
+            required
+            v-model="name"
+        /></label>
       </sui-form-field>
       <sui-form-field>
         <label
-          >Email <input type="text" id="email" placeholder="Email" />
+          >Email
+          <input
+            type="email"
+            data-email
+            placeholder="Email"
+            required
+            v-model="email"
+          />
         </label>
       </sui-form-field>
       <sui-form-field>
         <label>
-          Password <input type="text" id="password" placeholder="password" />
+          Password
+          <input
+            type="password"
+            data-password
+            placeholder="password"
+            required
+            v-model="password"
+          />
         </label>
       </sui-form-field>
       <sui-button primary type="submit">Create User</sui-button>
     </sui-form>
+    <sui-message v-if="submitted">
+      <sui-message-header>Success</sui-message-header>
+      <p message>User has been created</p>
+    </sui-message>
   </div>
 </template>
+
 <script>
 export default {
-  name: 'CreateUser'
+  name: 'CreateUser',
+  data() {
+    return {
+      submitted: false,
+      name: '',
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    handleSubmit() {
+      console.log(this.name, this.email, this.password);
+      this.submitted = true;
+    }
+  }
 };
 </script>
+
 <style></style>
--- tests/unit/user.test.js
+++ tests/unit/user.test.js
@@ -1,21 +1,37 @@
-import { shallowMount } from '@vue/test-utils';
+import { mount, createLocalVue } from '@vue/test-utils';
 import CreateUser from '../../src/components/CreateUser';
+import SuiVue from 'semantic-ui-vue';
+const localVue = createLocalVue();
+localVue.use(SuiVue);
 
 describe('CreateUser.vue', () => {
   it('Renders a form with name, email, and passworld fields', () => {
-    const wrapper = shallowMount(CreateUser);
-    expect(wrapper.find('#name').exists()).toBe(true);
-    expect(wrapper.find('#email').exists()).toBe(true);
-    expect(wrapper.find('#password').exists()).toBe(true);
+    const wrapper = mount(CreateUser, { localVue });
+    expect(wrapper.find('[data-name]').exists()).toBe(true);
+    expect(wrapper.find('[data-email]').exists()).toBe(true);
+    expect(wrapper.find('[data-password]').exists()).toBe(true);
+  });
+
+  it('reveals a notification when submitted', () => {
+    const wrapper = mount(CreateUser, { localVue });
+
+    wrapper.find('[data-name]').setValue('Peter');
+    wrapper.find('[data-email]').setValue('peter@example.com');
+    wrapper.find('[data-password]').setValue('12345678');
+    wrapper.find('[form]').trigger('submit.prevent');
+
+    expect(wrapper.find('[message]').text()).toBe('User has been created');
+  });
+
+  it('adds a user', async () => {
+    const wrapper = mount(CreateUser, { localVue });
+
+    wrapper.find('[data-name]').setValue('Peter');
+    wrapper.find('[data-email]').setValue('peter@example.com');
+    wrapper.find('[data-password]').setValue('12345678');
+
+    const response = await wrapper.vm.handleSubmit();
+    expect(response.body).not.toBeNull();
+    expect(response.body.name).toBe('Peter');
   });
 });
-
-// describe("HelloWorld.vue", () => {
-//   it("renders props.msg when passed", () => {
-//     const msg = "new message";
-//     const wrapper = shallowMount(HelloWorld, {
-//       propsData: { msg }
-//     });
-//     expect(wrapper.text()).toMatch(msg);
-//   });
-// });
